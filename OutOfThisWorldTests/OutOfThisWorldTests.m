88b0b8e314064a4e26728ec8d5d1ec75ad969541
Eliza Cooper <Eliza@Elizas-MacBook-Air.local>
2013-10-23 12:20:54 -0400

    Initial Commit

---
+++ OutOfThisWorldTests/OutOfThisWorldTests.m
@@ -0,0 +1,34 @@
+//
+//  OutOfThisWorldTests.m
+//  OutOfThisWorldTests
+//
+//  Created by Eliza Cooper on 10/23/13.
+//  Copyright (c) 2013 Eliza Cooper. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OutOfThisWorldTests : XCTestCase
+
+@end
+
+@implementation OutOfThisWorldTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
